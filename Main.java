1c2ea2e0fe2f23270328d284216e7734f2aff74e
Siddhant Sanadhaya <sid.sanadhaya@gmail.com>
2024-03-04 16:27:01 +0530

    java basics

---
+++ Main.java
@@ -0,0 +1,13 @@
+//Every class in java extends object class
+// any data type which can not be break even further String is not, char is a primitive datatype
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.next()+1);
+        
+    }
+    
+}
+ 
---
+++ Sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Sum{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number");
+        int num_1 = input.nextInt();
+        System.out.print("Enter another number");
+        int num_2 = input.nextInt();
+
+        int sum = num_1+num_2;
+        System.out.println(sum);
+    }
+}
